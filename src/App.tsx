de3c50f23dc96152f87985133534ca8e49e887ec
MuhammedEminCanbaz <emincanbazk@gmail.com>
2025-08-21 16:32:41 +0300

    project started and basic folder structure created

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import RouterConfig from './config/RouterConfig'
+
+function App() {
+
+  return (
+    <RouterConfig />
+  )
+}
+
+export default App
